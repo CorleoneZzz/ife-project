0f505314d76f9d4eef7ae65275c418e85daf36f5
CorleoneG <1410131029@qq.com>
2019-07-11 18:14:00 +0800

    js-回调函数

--- Callback/base.js
+++ Callback/base.js
@@ -1,11 +1,26 @@
 //本质就是在主线执行过程中插入执行代码
-function yo(callback) {
-    console.log("Yo")
-    if (callback) {
-        callback();
+// function yo(callback) {
+//     console.log("Yo")
+//     if (callback) {
+//         callback();
+//     }
+// }
+//
+// yo(function () {
+//     console.log("我甘地，打钱")
+// })
+
+let a = [2, 4, 6, 8, 10];
+
+function each(a, callback) {
+    for (let i = 0; i < a.length; i++) {
+        let item = a[i];
+        callback(item);
     }
 }
 
-yo(function () {
-    console.log("我甘地，打钱")
-})
+each(a, function (item) {
+    if (item > 5) {
+        console.log(item)
+    }
+});
